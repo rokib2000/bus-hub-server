2679d5148b5754b56be29cc78bd422e53f315d8e
rokib2000 <rokib0w1@gmail.com>
2022-11-27 01:57:02 +0600

    bus hub project setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Bus Hub Server running");
+});
+
+app.listen(port, () => {
+  console.log(`server running on  port ${port}`);
+});
