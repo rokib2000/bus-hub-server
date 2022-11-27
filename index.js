109afb841bb0584d382c1fc21ef13d1bae9a4d90
rokib2000 <rokib0w1@gmail.com>
2022-11-27 13:19:19 +0600

    database added and get category data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +9,33 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASS;
+
+const uri = `mongodb+srv://${db_user}:${db_password}@cluster0.d4mp0ot.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const categoryCollection = client.db("busHub").collection("categories");
+
+    // categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const cursor = categoryCollection.find(query);
+      const categories = await cursor.toArray();
+      res.send(categories);
+    });
+  } finally {
+    //finally
+  }
+}
+run();
 
 app.get("/", (req, res) => {
   res.send("Bus Hub Server running");
