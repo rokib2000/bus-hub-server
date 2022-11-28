9831a175d95573a5cdcff4a2b373dd6b6a528dcc
rokib2000 <rokib0w1@gmail.com>
2022-11-29 00:31:45 +0600

    delete reported product

--- index.js
+++ index.js
@@ -139,6 +139,14 @@ async function run() {
       res.send(result);
     });
 
+    //product delete
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     //************** */
     //Order
     //************** */
